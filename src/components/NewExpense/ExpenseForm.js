79226d45e9a44350c34d32e9f4b653eef9fc1209
Onlynfk <onlygod15@hotmail.com>
2021-05-03 00:46:43 +0100

    😊 empty form after submit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -17,22 +17,39 @@ const ExpenseForm =  () =>{
     const dateChangeHandler = event => {
         setselectedDate(event.target.value);
     };
-    return <form>
+
+    const submitHandler = (event) =>{
+        event.preventDefault();
+
+        const expenseData = {
+            title :enteredTitle,
+            amount : enteredAmount ,
+            date : new Date(selectedDate),
+        };
+
+        console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setselectedDate('');
+    };
+
+
+    return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label >Title</label>
-                <input type="text" onChange={titleChangeHandler} />
+                <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
             </div>
 
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
             </div>
 
             
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+                <input type="date" min="2019-01-01" max="2022-12-31" value={selectedDate} onChange={dateChangeHandler}/>
             </div>
 
             
