5b056e5515b4e7df5e73eaddf4635e165e6c0b35
Onlynfk <onlygod15@hotmail.com>
2021-05-02 22:09:21 +0100

    👀 usestate added

--- src/components/Expenses/ExpenseItem/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem/ExpenseItem.js
@@ -1,18 +1,25 @@
+import React, {useState} from 'react';
 import './ExpenseItem.css';
 import Card from '../../UI/Card';
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 
 
 const ExpenseItem = (props) =>{
- const clickHandler =() =>{
-    console.log("clicked");
- }
+// function check handler
+    const [title, setTitle] = useState(props.title);
+    console.log("ExpenseItem re-evaluated", )
+    // let title = props.title;
+
+    const clickHandler =() =>{
+        setTitle("Updated!");
+        console.log(title);
+    };
 
     return ( 
         <Card className="expense-item ">
           <ExpenseDate date = {props.date}/>
           <div className="expense-item__description">
-          <h2>{props.title}</h2>
+          <h2>{title}</h2>
           <div className="expense-item__price">
               ${props.amount}
           </div>
